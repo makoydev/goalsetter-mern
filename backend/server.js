1d03f9fa98a7d76edafea85bd1d0d2e54765c1ad
Michael Mendoza <engr.chaelmendoza@gmail.com>
2022-06-06 14:47:05 +0800

    feat: add goal controller

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,13 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 
+app.use(express.json())
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+)
+
 app.use('/api/goals', require('./routes/goalRoutes'))
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`))
