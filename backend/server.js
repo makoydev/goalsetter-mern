d3149d9eacb586a0c7df5987521296961d073420
Michael Mendoza <engr.chaelmendoza@gmail.com>
2022-06-07 19:54:05 +0800

    feat: done backend

--- backend/server.js
+++ backend/server.js
@@ -3,21 +3,31 @@ const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
-const PORT = process.env.PORT || 5000
+const port = process.env.PORT || 5000
 
 connectDB()
 
 const app = express()
 
 app.use(express.json())
-app.use(
-  express.urlencoded({
-    extended: false,
-  })
-)
+app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/goals', require('./routes/goalRoutes'))
+app.use('/api/users', require('./routes/userRoutes'))
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  )
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'))
+}
 
 app.use(errorHandler)
 
-app.listen(PORT, () => console.log(`Server started on ${PORT}`))
+app.listen(port, () => console.log(`Server started on port ${port}`))
